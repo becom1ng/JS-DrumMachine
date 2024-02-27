2a8f0f8da27adf2b829194eba4a5385372f3cfd5
becom1ng <becom1ng@users.noreply.github.com>
2024-02-27 10:07:41 -0600

    Audio playback, key styling

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ function playSound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-	console.log(audio, key);
+	if (!audio) return;
+
+	audio.currentTime = 0;
+	audio.play();
+
+	key.classList.add('playing');
+	setTimeout(() => {
+		key.classList.remove('playing');
+	}, 100);
 }
